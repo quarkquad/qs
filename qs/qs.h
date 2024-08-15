2e25477b8ffbc92a191756407304ee11d165d0b5
Brett Buddin <brett@buddin.org>
2024-08-14 21:49:47 -0400

    Prepare for release.

--- qs/qs.h
+++ qs/qs.h
@@ -8,7 +8,7 @@ BEGIN_JUCE_MODULE_DECLARATION
 
 ID:                 qs
 vendor:             dublab
-version:            0.1.0
+version:            0.1.1
 name:               qs
 description:        QS encoder and decoder.
 website:            https://dublab.com
